a78e9b3ca8aa5c47488abe4bd71f236c5e9fea65
filipecarvas <filipecarvas@hotmail.com>
2013-07-29 19:06:40 +0100

    Mais uma

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -49,7 +49,10 @@ function bt_AvailableClick()
 	document.getElementById("span-available").style.backgroundColor = "rgb(135,187,83)";
 	status = "Disponivel";
 	// Verificar se div do mapa existe
-	if (document.getElementById("map"))
+	if (!document.getElementById("map"))
+	{
+		getLocation();
+	} else 
 	{
 		// Remover div mapa
 		var div = document.getElementById("map");
@@ -70,7 +73,10 @@ function bt_BusyClick()
 	document.getElementById("span-busy").style.boxShadow = "inset 0px 1px 0px 0px rgba(250,250,250,0.5), 0px 0px 3px 2px rgba(226,0,0,0.5)";
 	document.getElementById("span-busy").style.backgroundColor = "rgb(226,0,0)";
 	status = "Ocupado";
-	if (document.getElementById("map"))
+	if (!document.getElementById("map"))
+	{
+		getLocation();
+	} else 
 	{
 		// Remover div mapa
 		var div = document.getElementById("map");

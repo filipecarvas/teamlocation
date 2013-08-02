db25e174ffcfa0f2677638706a7582500c5bc41c
filipecarvas <filipecarvas@hotmail.com>
2013-08-02 11:39:17 +0100

    Ajustes

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -504,19 +504,26 @@ function GetUserLocation()
 		break;
 	}
 
+	var mapOptions = {
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+				
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);						
+
 	var pos = new google.maps.LatLng(_Latitude, _Longitude);
-	var options = {
-			zoom: 17,
-			center: pos,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		
-		var map = new google.maps.Map(document.getElementById("mapUser"), options);
-		var marker = new google.maps.Marker({
-			position: pos,
-			map: map,
-			title: "User location"
-		});
+
+	var marker = new google.maps.Marker({
+		map: map,
+		position: pos,
+		title: 'User Location.'
+	});
+
+	map.setCenter(pos);
+	var infowindow = new google.maps.InfoWindow(options);
+	map.setCenter(options.position);
+			
+	google.maps.event.addDomListener(window, 'load', GetUserLocation);
 }
 
 function AlterarEmailLog(OldEmail, NewEmail)
